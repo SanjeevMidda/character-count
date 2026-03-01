c5e32dc4311e234f2f7dfc60a7ab3532d8977cd2
Sanjeev Midda <s.midda1@gmail.com>
2026-03-01 12:57:49 +0000

    created component to handle input and the number of characters entered

---
+++ src/components/Input.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Input = () => {
+  return (
+    <div className="inputContainer">
+      <input type="text" />
+      <h3 id="numberCount"> 12 characters entered</h3>
+    </div>
+  );
+};
+
+export default Input;
