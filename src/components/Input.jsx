11a40cf29542c7c61dbb8dbc781af35a421da124
Sanjeev Midda <s.midda1@gmail.com>
2026-03-01 13:57:51 +0000

    added default value for inputLimit to avoid error if no value passed

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const Input = ({ inputLimit }) => {
+const Input = ({ inputLimit = 30 }) => {
   const [userInput, setUserInput] = useState("");
 
   const handleChange = ({ target }) => setUserInput(target.value);
@@ -11,7 +11,9 @@ const Input = ({ inputLimit }) => {
     } else if (userInput.length === 0) {
       return <p>No characters entered</p>;
     } else {
-      return <p>{`${userInput.length} characters entered`}</p>;
+      return (
+        <p aria-live="polite">{`${userInput.length} characters entered`}</p>
+      );
     }
   };
 
